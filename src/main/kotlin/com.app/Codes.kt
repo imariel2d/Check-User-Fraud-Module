eabaa950a93507b7c2cb49b03447449fa7a94e12
imariel2d <imariel2d@gmail.com>
2021-02-18 13:15:26 -0700

    replacing list with HashMap and removing code after X seconds.

--- src/main/kotlin/com.app/Codes.kt
+++ src/main/kotlin/com.app/Codes.kt
@@ -1,14 +1,29 @@
 package com.app
 
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.concurrent.schedule
+
 object Codes {
-    val codes = mutableListOf<Code>()
+    val codes = HashMap<Int, Int>();
 
     fun insertCode(code: Code) {
         println("Saving ${code.code} in memory...")
-        codes.add(code)
+        codes[code.userId] = code.code
+
+        Timer("schedule", true).schedule(15000) {
+            removeCode(code)
+        }
     }
 
-    fun removeCode(code: Code) {
-        codes.remove(code)
+    fun removeCode(code: Code): Int? {
+        val userCode = codes[code.userId]
+
+        if (userCode === code.code) {
+            println("Removing code.")
+            return codes.remove(code.userId)
+        } else {
+            return null
+        }
     }
 }
--- src/main/kotlin/com.app/Verifier.kt
+++ src/main/kotlin/com.app/Verifier.kt
@@ -2,13 +2,7 @@ package com.app
 
 class Verifier {
   fun verifyCode(code: Code): Boolean {
-    Codes.codes.forEach {
-      if (it.code === code.code && it.userId === code.userId) {
-        Codes.removeCode(it)
-        return true
-      }
-    }
-
-    return false
+    val removedCode = Codes.removeCode(code)
+    return removedCode !== null
   }
 }
